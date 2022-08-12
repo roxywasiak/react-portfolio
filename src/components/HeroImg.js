f412692e3e58fc2d82623f3c39aab6de54189be7
roxywasiak <ruksclone@hotmail.com>
2022-08-12 18:05:58 +0100

    experimenting with backgrounds

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,12 +1,12 @@
 import "./HeroImgStyles.css";
 import React from "react";
-import HeroImage from "../assets/hero-img.jpg";
+import HeroImage from "../assets/sunshine.jpg";
 
 const HeroImg = () => {
   return (
     <div className="hero">
       <div className="mask">
-        <img className="hero-img" src={HeroImage} alt="Sky" />
+        <img className="hero-img" src={HeroImage} alt="Lady" />
       </div>
     </div>
   );
