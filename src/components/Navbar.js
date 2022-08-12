e97a0a46457bc59d4d2423e116e3c3de4a3990d8
roxywasiak <ruksclone@hotmail.com>
2022-08-12 17:35:37 +0100

    added click function to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,19 @@
 import "../components/NavbarStyles.css";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+// icons from react and need to specialize which category
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
     <div className="header">
       <Link to="/">
         <h1>Portfolio</h1>
       </Link>
-      <ul className="nav-menu">
+      <ul className={click ? "nav-menu.active" : "nav-menu"}>
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -22,6 +27,15 @@ const Navbar = () => {
           <Link to="/contact">Contact</Link>
         </li>
       </ul>
+      k
+      <div className="hamburger" onClick={handleClick}>
+        {/* to show or not show close icon for menu */}
+        {click ? (
+          <FaTimes size={20} style={{ color: "#fff" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "#fff" }} />
+        )}
+      </div>
     </div>
   );
 };
