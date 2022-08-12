1943c6a293c178838d8d9bb5c17255c2889253b9
roxywasiak <ruksclone@hotmail.com>
2022-08-12 15:52:55 +0100

    hooked up all the pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import react from "react";
 import "./index.css";
+import Home from "./routes/Home";
+import Project from "./routes/Project";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+//Route the pages into function using path
+//import routes
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>This is my App</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="about/" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
