1c86cbd902813c2eb0fec92315fc118963e822d3
roxywasiak <ruksclone@hotmail.com>
2022-08-14 21:35:46 +0100

    added next project page items

---
+++ src/components/HeroTwo.js
@@ -0,0 +1,15 @@
+import "./HeroTwoStyles.css";
+import React from "react";
+
+const HeroTwo = () => {
+  return (
+    <div className="hero-img">
+      <div className="heading">
+        <h1>This is my heading</h1>
+        <p>some text</p>
+      </div>
+    </div>
+  );
+};
+
+export default HeroTwo;
