09335d612d0fa79022ff356e389dcb390c3d4e71
roxywasiak <ruksclone@hotmail.com>
2022-08-14 20:44:23 +0100

    added footer components and styling

---
+++ src/components/Footer.js
@@ -0,0 +1,71 @@
+import "./FooterStyles.css";
+import React from "react";
+import {
+  FaHome,
+  FaRegEnvelopeOpen,
+  FaPhone,
+  FaFacebook,
+  FaLinkedin,
+  FaInstagram,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+            <FaHome size={20} style={{ color: "#ffff", marginRight: "2rem" }} />
+          </div>
+
+          <div>
+            <p>58 Lyttleton Street</p>
+            <p>West Bromwhich</p>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "#ffff", marginRight: "2rem" }}
+              />
+              07908813491
+            </h4>
+          </div>
+
+          <div className="email">
+            <h4>
+              <FaRegEnvelopeOpen
+                size={20}
+                style={{ color: "#ffff", marginRight: "2rem" }}
+              />
+              ruksclone@hotmail.com
+            </h4>
+          </div>
+        </div>
+
+        <div className="right">
+          <h4>Created By Rukhsana Wasiak</h4>
+          <p>
+            Contact me on any of my socials or email for any more information
+          </p>
+          <div className="social">
+            <FaFacebook
+              size={30}
+              style={{ color: "#ffff", marginRight: "1rem" }}
+            />
+            <FaLinkedin
+              size={30}
+              style={{ color: "#ffff", marginRight: "1rem" }}
+            />
+            <FaInstagram
+              size={30}
+              style={{ color: "#ffff", marginRight: "1rem" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
