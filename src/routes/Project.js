80d0daa24d745e6b78e936dfa1c8aa59c570393f
roxywasiak <ruksclone@hotmail.com>
2022-08-15 20:49:25 +0100

    added images to add to workcards

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import HeroTwo from "../components/HeroTwo";
+import WorkCard from "../components/WorkCard";
 
 const Project = () => {
   return (
     <div>
       <Navbar />
       <HeroTwo heading="PROJECTS" text="Some of my most recent work" />
+      <WorkCard />
       <Footer />
     </div>
   );
