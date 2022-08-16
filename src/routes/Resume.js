8ddda867ff8211afcf45fe1ab70951b599998209
roxywasiak <ruksclone@hotmail.com>
2022-08-16 18:34:08 +0100

    Added resume styles

--- src/routes/Resume.js
+++ src/routes/Resume.js
@@ -2,19 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import HeroTwo from "../components/HeroTwo";
-import { Link } from "react-router-dom";
-import { FaFileDownload } from "react-icons/fa";
+import Cv from "../components/Cv";
 
 const Resume = () => {
   return (
     <div>
       <Navbar />
       <HeroTwo heading="RESUME" text="Explore my resume" />
-      <div className="cv">
-        <Link to="./src/assets/rukhsana.pdf" target="_blank" download>
-          <FaFileDownload size={80} style={{ color: "#fff" }} />
-        </Link>
-      </div>
+      <CvFile />
       <Footer />
     </div>
   );
