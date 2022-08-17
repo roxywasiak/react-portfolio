e03e462a7eeb81aa1dff05bd834ca1ef8fbd22d1
roxywasiak <ruksclone@hotmail.com>
2022-08-17 19:36:59 +0100

    fixed cv position but needs raising higher

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -1,17 +1,17 @@
 import "./CvStyles.css";
-import Cv from "./Cv";
 import { FaFileDownload } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 import React from "react";
 
 const CV = () => {
-  return;
-  <div className="cv">
-    <Link to="./src/assets/rukhsana.pdf" target="_blank" download>
-      <FaFileDownload size={80} style={{ color: "#fff" }} />
-    </Link>
-  </div>;
+  return (
+    <div className="cv">
+      <Link to="./src/assets/rukhsana.pdf" target="_blank" download>
+        <FaFileDownload size={80} style={{ color: "#fff" }} />
+      </Link>
+    </div>
+  );
 };
 
 export default CV;
--- src/routes/Resume.js
+++ src/routes/Resume.js
@@ -2,14 +2,16 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import HeroTwo from "../components/HeroTwo";
-import Cv from "../components/Cv";
+import { Link } from "react-router-dom";
+
+import CV from "../components/Cv";
 
 const Resume = () => {
   return (
     <div>
       <Navbar />
       <HeroTwo heading="RESUME" text="Explore my resume" />
-      <CvFile />
+      <CV />
       <Footer />
     </div>
   );
