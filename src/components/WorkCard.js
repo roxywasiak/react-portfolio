4f8b19e93d1c91a549973674103d0327c10eb66f
roxywasiak <ruksclone@hotmail.com>
2022-08-17 20:55:21 +0100

    created initial dataset for card map function

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -6,23 +6,18 @@ import { NavLink } from "react-router-dom";
 
 const WorkCard = () => {
   return (
-    <div className="work-container">
-      <h1 className="project-heading">Projects</h1>
-      <div className="project-container">
-        <div className="project-card">
-          <img src={pro1} alt="project image examples" />
-          <h2 className="project-title">Project Title</h2>
-          <div className="pro-details">
-            <p>This is text</p>
-            <div className="pro-btns">
-              <NavLink to="url.com" className="btn">
-                View
-              </NavLink>
-              <NavLink to="url.com" className="btn">
-                Source
-              </NavLink>
-            </div>
-          </div>
+    <div className="project-card">
+      <img src={pro1} alt="project image examples" />
+      <h2 className="project-title">Project Title</h2>
+      <div className="pro-details">
+        <p>This is text</p>
+        <div className="pro-btns">
+          <NavLink to="url.com" className="btn">
+            View
+          </NavLink>
+          <NavLink to="url.com" className="btn">
+            Source
+          </NavLink>
         </div>
       </div>
     </div>
---
+++ src/components/WorkCardData.js
@@ -0,0 +1,16 @@
+import pro1 from "../assets/buddyup.png";
+import pro2 from "../assets/homepage.png";
+import pro3 from "../assets/notehome.png";
+import pro4 from "../assets/workdaysched.png";
+import pro5 from "../assets/passwordgen.png";
+import pro6 from "../assets/quizstart.png";
+
+const ProjectCardData = [
+  {
+    imgsrc: pro1,
+    title: "BuddyUp",
+    text: "This is an interactive app that allows students to find their ideal tutor to help further their skills in their chosen subject The app also allows students to find other students that they want to study with.We allow students to find their perfect match within their are or remotely. We allow students to post adds so potential tutors can respond we then render a match card for the student showing them they've been matched with a tutor.",
+    view: "https://limitless-citadel-05906.herokuapp.com/",
+    source: "https://github.com/roxywasiak/buddy-up",
+  },
+];
