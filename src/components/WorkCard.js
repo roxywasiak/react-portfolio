8583edd753831ec60abf7ddb8a0eb00cc0f31e66
roxywasiak <ruksclone@hotmail.com>
2022-08-17 19:51:55 +0100

    added card classNames to start styles

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,6 +1,8 @@
 import "./WorkCardStyles.css";
+import pro1 from "../assets/buddyup.png";
 
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const WorkCard = () => {
   return (
@@ -8,7 +10,19 @@ const WorkCard = () => {
       <h1 className="project-heading">Projects</h1>
       <div className="project-container">
         <div className="project-card">
-          <img src="" alt="images" />
+          <img src={pro1} alt="project image examples" />
+          <h2 className="project-title">Project Title</h2>
+          <div className="pro-details">
+            <p>This is text</p>
+            <div className="pro-btns">
+              <NavLink to="url.com" className="btn">
+                View
+              </NavLink>
+              <NavLink to="url.com" className="btn">
+                Source
+              </NavLink>
+            </div>
+          </div>
         </div>
       </div>
     </div>
