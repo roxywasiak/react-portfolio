78ee12857abf9b373fc2a8842e38d8510b454170
roxywasiak <ruksclone@hotmail.com>
2022-08-22 20:59:16 +0100

    completed form function with library

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,39 @@
 import "./FormStyles.css";
-
-import React from "react";
+import emailjs from "@emailjs/browser";
+import React, { useRef } from "react";
 
 const Form = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_l77jz4t",
+        "template_jxbit8e",
+        form.current,
+        "cKB-x9rYkmrRofXdS"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div className="form">
-      <form>
-        <label>YourName</label>
-        <input type="text"></input>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
         <label>Email</label>
-        <input type="email"></input>
-        <label>Subject</label>
-        <input type="text"></input>
+        <input type="email" name="user_email" />
         <label>Message</label>
-        <textarea rows="6" placeholder="Type your message here" />
+        <textarea name="message" />
         <button className="btn">Submit</button>
       </form>
     </div>
