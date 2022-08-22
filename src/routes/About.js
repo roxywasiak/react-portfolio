2aba851bfe728571bb5d60f372f7cf2d4d4b8fff
roxywasiak <ruksclone@hotmail.com>
2022-08-22 21:17:32 +0100

    fmedia query for photo image

--- src/routes/About.js
+++ src/routes/About.js
@@ -10,10 +10,11 @@ const About = () => {
       <Navbar />
       <HeroTwo
         heading="About Me"
-        text="Rukhsana Wasiak"
+        text=" 👋 Rukhsana Wasiak"
         subhead="A collaborative Full Stack Developer based in Birmingham. 
-        I come from a strong client facing background and enjoy problem solving. I enjoy cookery and fitness in my spare time as well as learning more code to build new projects."
+        I come from a strong client facing background and enjoy problem solving. I enjoy coding cookery and fitness in my spare time."
       />
+
       <div className="photo-place">
         <img className="photo" src={PhotoImage} alt="Image of a lady" />
       </div>
