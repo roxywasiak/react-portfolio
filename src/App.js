0d68a1206b5dccd7198adc75da0dad7d772122f7
roxywasiak <ruksclone@hotmail.com>
2022-08-27 15:14:19 +0100

    about page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path="/about" element={<About exact={true} />} />
         <Route path="/" element={<Home exact={true} />} />
         <Route path="/project" element={<Project exact={true} />} />
         <Route path="/about" element={<About exact={true} />} />
